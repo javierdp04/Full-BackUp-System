f43e02a29930d173f734b499f347d46df6ff69ef
Javier <javierdepablos04@gmail.com>
2026-06-20 23:04:41 +0200

    dbTables + filesSelect

---
+++ server/DB/DBAPI.ts
@@ -0,0 +1,15 @@
+import mysql from 'mysql';
+import getEnvVariable from '../scripts/env_utils.js';
+
+const connection = mysql.createConnection({
+    host : await getEnvVariable("DB_URL"),
+    user : await getEnvVariable("DB_USERNAME"),
+    password : await getEnvVariable("DB_USERPASSWORD"),
+    database : await getEnvVariable("DB_NAME")
+});
+
+connection.connect((error) => {
+    if (error) Error(`Can't connect to the database ${error}`)
+})
+
+export default connection;
